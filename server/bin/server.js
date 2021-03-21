8865b7aa4663cd571ae2766a32083586e73e26c2
gorushkin <gorushkin@gmail.com>
2021-03-21 16:22:06 +0300

    чиним конфиг

--- server/bin/server.js
+++ server/bin/server.js
@@ -6,8 +6,7 @@ import getApp from '../index.js';
 dotenv.config();
 
 const port = process.env.PORT || 5000;
-// const address = process.env.ADDRESS;
-const address = '0.0.0.0';
+const address = process.env.ADDRESS;
 
 getApp().listen(port, address, () => {
   console.log(`Server is running on port: ${port}`);
