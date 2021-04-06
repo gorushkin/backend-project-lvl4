32f8b8006bcec44a781b97b9993447701a304089
gorushkin <gorushkin@gmail.com>
2021-04-06 10:46:38 +0300

    indent

--- server/migrations/20210124105507_add_first_and_last_name_to_usres_table.js
+++ server/migrations/20210124105507_add_first_and_last_name_to_usres_table.js
@@ -1,13 +1,13 @@
   // @ts-check
 
-  exports.up = (knex) => (
-    knex.schema.table('users', (table) => {
-      table.string('first_name');
-      table.string('last_name');
-    })
-  );
+exports.up = (knex) => (
+  knex.schema.table('users', (table) => {
+    table.string('first_name');
+    table.string('last_name');
+  })
+);
 
-  exports.down = (knex) => knex.schema.table('users', (table) => {
-    table.dropColumn('first_name');
-    table.dropColumn('last_name');
-  });
+exports.down = (knex) => knex.schema.table('users', (table) => {
+  table.dropColumn('first_name');
+  table.dropColumn('last_name');
+});
